b572dc8262fe88188c5ce7ef661bea5e98dbe31b
vanesaespin <vespin@ieshlanz.es>
2023-02-03 18:28:06 +0100

    ultimos estados funcionando

--- listadeseos/src/App/ListaDeseos/DeseoItem.jsx
+++ listadeseos/src/App/ListaDeseos/DeseoItem.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import classNames from "classnames";
 
-const DeseoItem = ({texto, cumplido, onCumplidoChange, id}) =>{
-  
-    return (
+const DeseoItem = ({texto, cumplido, id, onCumplidoChange}) =>
         <li className={classNames(
-            'lista-deseos__item',
-            { 'lista-deseos__item--cumplido': cumplido}
+            'lista-deseos__item',{ 'lista-deseos__item--cumplido': cumplido}
         )}>
-            <input id={id} type="checkbox" 
-                    checked={cumplido} 
-                    onChange={e => onCumplidoChange(e.target.checked)} />
-            
+            <input id={id} type="checkbox" checked={cumplido} 
+                 onChange={e => onCumplidoChange(e.target.checked)} />          
             <label htmlFor={id}>{texto}</label>
         </li>
-    );
-}
+  
+
+
+// const DeseoItem = ({texto, cumplido, onCumplidoChange, id}) =>{
+  
+//     return (
+//         <li className={classNames(
+//             'lista-deseos__item',
+//             { 'lista-deseos__item--cumplido': cumplido}
+//         )}>
+//             <input id={id} type="checkbox" 
+//                     checked={cumplido} 
+//                     onChange={e => onCumplidoChange(e.target.checked)} />
+            
+//             <label htmlFor={id}>{texto}</label>
+//         </li>
+//     );
+// }
 export default DeseoItem;
