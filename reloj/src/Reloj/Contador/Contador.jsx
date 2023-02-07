06129a25456b1a3bde35bdedda71bc45782f7795
vanesaespin <vespin@ieshlanz.es>
2023-02-07 11:32:47 +0100

    Proyecto Reloj con UseEffects

---
+++ reloj/src/Reloj/Contador/Contador.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+const Contador = ({intervalo}) => {
+    const [conta, setConta]=useState(0);
+    useEffect (() => {
+        const intervalo = setInterval ( ()=> setConta (c=>c+1), 1000)            
+        return () => clearInterval (intervalo);
+    
+    }, []);
+
+    return <p>{conta}</p>;
+}
+
+/*VERSION DONDE EL USEEFFECT DEPENDE DEL CONTADOR CONTA*/
+// const Contador = () => {
+//     const [conta, setConta]=useState(0);
+//     useEffect (() => {
+//         const intervalo = setInterval ( ()=> setConta (conta+1), 1000)            
+//         return () => clearInterval (intervalo);
+    
+//     }, [conta]);
+
+//     return <p>{conta}</p>;
+// }
+
+export default Contador;
---
+++ reloj/src/Reloj/Reloj.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Contador from "./Contador";
+import { useEffect } from "react";
+
+/*COMPONENTE RELOJ QUE SE ACTUALIZA CADA SEGUNDO SIN BOTÓN*/
+
+const Reloj = () => {
+
+    const [now, setNow] = React.useState(new Date().toLocaleTimeString());
+
+    useEffect (()=>{
+        const tick = () => setNow(new Date().toLocaleTimeString()); 
+        const intervalo = setInterval (tick, 1000);
+        return () => clearInterval (intervalo);
+    },[]);
+
+    
+
+    return (
+        <div className="container">
+            <span className="clock"> {now} </span>
+            <br />          
+            <Contador intervalo={0}/>
+        </div> 
+    )
+
+}
+
+
+/*COMPONENTE RELOJ CON BOTON Y UN CONTADOR*/
+// const Reloj = () => {
+
+//     const [now, setNow] = React.useState(new Date().toLocaleTimeString());
+
+//     return (
+//         <div className="container">
+//             <span className="clock"> {now} </span>
+//             <br />
+//             <button className="boton"
+//                     onClick={() => { setNow(new Date().toLocaleTimeString()); }}> Dame la hora </button>
+//         <Contador/>
+//         </div>       
+//     )
+// }
+
+export default Reloj;
