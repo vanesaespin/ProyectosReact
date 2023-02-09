bb165580642142471570449db2bdd07d6f1c6e54
vanesaespin <vespin@ieshlanz.es>
2023-02-09 19:25:37 +0100

    uso de children en api de consejos

--- leer_api_consejos/src/App.js
+++ leer_api_consejos/src/App.js
@@ -1,39 +1,41 @@
 import {useState, useEffect} from "react"
 import './App.css';
+import MarcoDeseos from "./components/MarcoDeseos";
 
 function App() {
-  const [consejo, setConsejo] = useState("");
+  //hook de estado para el consejo
+  const [consejo, setConsejo] = useState([]);
 
   useEffect(() => {
      //Llamamos a fetchData
-    fetchData2();
+    fetchData();
   }, []);
 
   //Forma 1: promesa pura --> te dice que lo conviertas a async
-  // const fetchData1 = () => {
-  //   return fetch("https://api.adviceslip.com/advice")
-  //         .then((response) => response.json())
-  //         .then((data) => console.log(data));
-  // }
+  /* const fetchData1 = () => {
+     return fetch("https://api.adviceslip.com/advice")
+           .then((response) => response.json())
+           .then((data) => console.log(data));
+   } */
 
   //Forma 2: con promesa dentro de async
-  const fetchData2 = async () => { //Función asíncrona que espera a que se resuelva la promesa
+  const fetchData = async () => { //Función asíncrona que espera a que se resuelva la promesa
     try {
       const url = "https://api.adviceslip.com/advice"; //URL de la API que da consejos
-        const response = await fetch(url); //Llamamos a la API y esperamos la respuesta de pasar a la siguiente linea
-        const json = await response.json(); //Transformamos la respuesta a json
-        setConsejo(json.slip.advice); //La pintamos por consola
+      const response = await fetch(url); //Llamamos a la API y esperamos la respuesta de pasar a la siguiente linea
+      const json = await response.json(); //Transformamos la respuesta a json
+      setConsejo(json.slip); //La pintamos por consola
         
     } catch (error) {
         console.log("error", error);
     }
-};
+  };
  
   return (
-    <div>
-      <h1>Consejo: {consejo}</h1>
-       
-    </div>
+    <MarcoDeseos color="rojo">
+      <h1>Consejo: {consejo.id}</h1> 
+      <h2> {consejo.advice}</h2>
+    </MarcoDeseos>
   );
 }
 
